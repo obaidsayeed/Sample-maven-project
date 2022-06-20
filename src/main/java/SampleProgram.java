bbf81d202776771c56d7efb3c3dae2a4c4d40123
ubaidullahmohammed <obaidsayeed91@gmail.com>
2022-06-20 12:36:03 +0530

    Initial commit

---
+++ src/main/java/SampleProgram.java
@@ -0,0 +1,5 @@
+public class SampleProgram {
+    public static void main(String arg[]){
+        System.out.println("hello world");
+    }
+}
---
+++ src/test/SampleProgramTest.java
@@ -0,0 +1,5 @@
+public class SampleProgramTest {
+    public static void main(String[] args) {
+        SampleProgram obj = new SampleProgram();
+    }
+}
